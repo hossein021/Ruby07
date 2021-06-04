9f2d478efa5e1d7a5c0ef4cc28dcac5a5cff1ad3
Hossein <hosseinsalmani1373@gmail.com>
2021-06-04 12:48:41 +0430

    Strings2

---
+++ main12.rb
@@ -0,0 +1,22 @@
+# عملگرهای رشته ای
+# puts "Hi"
+# puts "Hi \n Hi"
+# puts "Hi \nHi"
+# puts "Ali \t Ruby"
+# puts "Ali \t\t\t\t ruby \n Ruby"
+# puts "Ali \\ Ruby"
+# puts "Ali \* Ruby"
+
+a = "ali"
+# puts a * 5
+b = "ruby"
+puts a + b
+
+puts a == b
+puts a === b
+puts a =="Ali"
+puts b == "ruby"
+puts "Ali" == 77
+puts "Ruby" << "Language"
+puts a << "a"
+puts b << " language"
